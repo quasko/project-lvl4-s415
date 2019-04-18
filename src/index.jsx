0825782d3c286029b6ff5fe1110922abbbc375ed
Denis Pukhovskiy <d.pukhovskiy@cti.ru>
2019-04-18 16:17:46 +0300

    add procfile

--- src/index.jsx
+++ src/index.jsx
@@ -29,7 +29,7 @@ const store = createStore(
   compose(
     applyMiddleware(thunk),
     // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
 
